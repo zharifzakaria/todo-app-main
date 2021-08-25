83a0b039b89c1db4dafd2c691a46107347448178
zharifzakaria <zharifzakaria@gmail.com>
2021-08-25 16:44:57 +0800

    vue: view each item in the tasklist

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,82 @@
 <template>
   <header>
-      <h1>Todo</h1>
-      <img src="/images/icon-moon.svg" alt="switch to dark" />
-    </header>
+    <h1>Todo</h1>
+    <img src="/images/icon-moon.svg" alt="switch to dark" />
+  </header>
 
-    <section class="input">
-      <IconCircle class="input-circle" />
-      <input type="text" placeholder="Create a new todo.." />
-    </section>
+  <section class="input">
+    <IconCircle class="input-circle" />
+    <input
+      type="text"
+      placeholder="Create a new todo.."
+      v-model="newTaskInput"
+      @keyup.enter="addTask"
+    />
+  </section>
 
-    <section class="todos">
-      <ul>
-        <li class="complete">
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>Complete online Javascript course</p>
-          <IconDelete />
-        </li>
-        <li>
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>Jog around the park 3x</p>
-          <IconDelete />
-        </li>
-        <li>
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>10 minutes meditation</p>
-          <IconDelete />
-        </li>
-        <li>
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>Read for 1 hour</p>
-          <IconDelete />
-        </li>
-        <li>
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>Pick up groceries</p>
-          <IconDelete />
-        </li>
-        <li>
-          <IconCircle />
-          <input type="checkbox" class="sr-only" />
-          <p>Complete Todo App on Frontend Mentor</p>
-          <IconDelete />
-        </li>
-        <li>
-          <p>5 items left</p>
-          <p>Clear Completed</p>
-        </li>
-      </ul>
-    </section>
+  <section class="todos">
+    <ul>
+      <li v-for="item in taskList" :key="item">
+        <IconCircle />
+        <IconCheck />
+        <input type="checkbox" class="sr-only" />
+        <p>{{ item }}</p>
+        <IconDelete />
+      </li>
+      <li>
+        <p>{{ taskList.length }} items left</p>
+        <p>Clear Completed</p>
+      </li>
+    </ul>
+  </section>
 
-    <section class="filter">
-      <ul>
-        <li class="active">All</li>
-        <li>Active</li>
-        <li>Completed</li>
-      </ul>
+  <section class="filter">
+    <ul>
+      <li class="active">All</li>
+      <li>Active</li>
+      <li>Completed</li>
+    </ul>
 
-      <p class="instructions">Drag and drop to reorder list</p>
-    </section>
+    <p class="instructions">Drag and drop to reorder list</p>
+  </section>
 </template>
 
 <script>
-import IconCircle from './components/IconCircle.vue';
-import IconCheck from './components/IconCheck.vue';
-import IconDelete from './components/IconDelete.vue';
+import { reactive, toRefs } from "vue";
+import IconCircle from "./components/IconCircle.vue";
+import IconCheck from "./components/IconCheck.vue";
+import IconDelete from "./components/IconDelete.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     IconCircle,
     IconCheck,
     IconDelete,
-  }
-}
+  },
+  setup() {
+    const state = reactive({
+      newTaskInput: "",
+      taskList: [],
+    });
+
+    const addTask = () => {
+      state.taskList.push(state.newTaskInput);
+      state.newTaskInput = '';
+    };
+
+    return {
+      ...toRefs(state),
+      addTask,
+    };
+  },
+};
+
+// Complete online Javascript course
+// Jog around the park 3x
+// 10 minutes meditation
+// Read for 1 hour
+// Pick up groceries
+// Complete Todo App on Frontend Mentor
 
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/IconCheck.vue
+++ src/components/IconCheck.vue
@@ -1,13 +1,25 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+  <span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="8">
+      <path
+        fill="none"
+        stroke="#FFF"
+        stroke-width="2"
+        d="M1 4.304L3.696 7l6-6"
+      />
+    </svg>
+  </span>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+span {
+  background-color: #21d4fd;
+  background-image: linear-gradient(-50deg, #21d4fd 0%, #b721ff 100%);
+  padding: 1px 3px;
+  border-radius: 50%;
+}
 </style>
